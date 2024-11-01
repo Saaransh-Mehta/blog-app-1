b702f8064e1b81130800d8ecc61b31a6408104ed
Saaransh-Mehta <saaransh1621m@gmail.com>
2024-11-01 18:56:15 +0530

    Starting a new project blogapp

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from "react"
+
+function App() {
+
+
+  return (
+    <>
+    <div className="text-3xl flex justify-center text-slate-900">
+   
+
+    </div>
+    </>
+  )
+}
+
+export default App
